e59960b6f1bd17c2224754786575dd061305fe8e
Crhispo <cristianandrade2001@gmail.com>
2022-03-29 17:09:47 -0500

    Cambio en la visualizacion del modelo de admin

--- aplications/academy/admin.py
+++ aplications/academy/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Course
 
 # Register your models here.
+
+# admin.site.register(Course)
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    '''Admin View for '''
+
+    list_display = ('id','name','credits')
+    list_editable = ('name','credits')
