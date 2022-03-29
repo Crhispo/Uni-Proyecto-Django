8291fecbb09c3327c991307c5b715b82d812da42
Crhispo <cristianandrade2001@gmail.com>
2022-03-29 17:08:24 -0500

    Se ordena por nombre

--- aplications/academy/views.py
+++ aplications/academy/views.py
@@ -4,5 +4,5 @@ from .models import Course
 
 
 def home(request):
-    cursosListados = Course.objects.all()
+    cursosListados = Course.objects.all().order_by("-name")
     return render(request, "gestionCursos.html", {"cursos": cursosListados})
