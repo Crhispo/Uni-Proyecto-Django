fbf0c5448b7d1922f4cec7b0bfe7412c52eab73d
Crhispo <cristianandrade2001@gmail.com>
2022-03-29 14:41:00 -0500

    Se agrego la vista home

--- aplications/academy/views.py
+++ aplications/academy/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Course
 # Create your views here.
+
+
+def home(request):
+    cursosListados = Course.objects.all()
+    return render(request, "gestionCursos.html", {"cursos": cursosListados})
