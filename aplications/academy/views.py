0e056838caa31c645667b057c24754ebd76f0491
Crhispo <cristianandrade2001@gmail.com>
2022-03-30 13:32:39 -0500

    se creo una clase oara la vista del template

--- aplications/academy/views.py
+++ aplications/academy/views.py
@@ -1,3 +1,5 @@
+from django.views.generic import ListView
+from pydoc import classname
 from django.shortcuts import render
 from .models import Course
 # Create your views here.
@@ -6,3 +8,17 @@ from .models import Course
 def home(request):
     cursosListados = Course.objects.all().order_by("-name")
     return render(request, "gestionCursos.html", {"cursos": cursosListados})
+
+
+class CourseListView(ListView):
+    model = Course
+    template_name = 'gestionCursos.html'
+
+    def get_queryset(self):
+        Course.objects.all()
+        return super().get_queryset()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Gestion de cursos"
+        return context
