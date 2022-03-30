dff366eb655e19c559601ee41c004961163a7d9a
Crhispo <cristianandrade2001@gmail.com>
2022-03-30 13:31:17 -0500

    Se añadio el modelo "Teacher"

--- aplications/academy/admin.py
+++ aplications/academy/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, Teacher
 
 # Register your models here.
 
@@ -11,3 +11,10 @@ class CourseAdmin(admin.ModelAdmin):
 
     list_display = ('id','name','credits')
     list_editable = ('name','credits')
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    '''Admin View for '''
+
+    list_display = ('id','apellido_paterno','apellido_materno')
